582cae22a476a8a925ad87e1a6ce98bef336db65
Hsi <its.hsichen@gmail.com>
2026-05-09 12:19:13 +0800

    refactor(tsconfig): remove baseUrl and paths

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
-import Main from 'components/Main';
-import './index.css';
 import { createRoot } from 'react-dom/client';
+
+import Main from './components/Main';
+import './index.css';
 
 const container = document.getElementById('root');
 if (container) {

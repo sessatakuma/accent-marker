c926cbfade6cd25d5329230f439711f8f43af56a
Hsi <its.hsichen@gmail.com>
2026-05-16 15:26:48 +0800

    fix: place expand control after export actions

--- src/components/AccentEditor/components/ResultActions.tsx
+++ src/components/AccentEditor/components/ResultActions.tsx
@@ -84,52 +84,6 @@ export default function ResultActions({
                     </button>
                 </div>
 
-                <button
-                    className='action-button action-button-expand'
-                    onClick={onToggleExpanded}
-                    title={isExpanded ? t.collapseResult : t.expandResult}
-                    aria-label={isExpanded ? t.collapseResult : t.expandResult}
-                    type='button'
-                >
-                    {isExpanded ? (
-                        <svg
-                            xmlns='http://www.w3.org/2000/svg'
-                            width='24'
-                            height='24'
-                            viewBox='0 0 24 24'
-                            fill='none'
-                            stroke='currentColor'
-                            strokeWidth='2'
-                            strokeLinecap='round'
-                            strokeLinejoin='round'
-                            aria-hidden='true'
-                        >
-                            <path d='M8 3v3a2 2 0 0 1-2 2H3' />
-                            <path d='M21 8h-3a2 2 0 0 1-2-2V3' />
-                            <path d='M3 16h3a2 2 0 0 1 2 2v3' />
-                            <path d='M16 21v-3a2 2 0 0 1 2-2h3' />
-                        </svg>
-                    ) : (
-                        <svg
-                            xmlns='http://www.w3.org/2000/svg'
-                            width='24'
-                            height='24'
-                            viewBox='0 0 24 24'
-                            fill='none'
-                            stroke='currentColor'
-                            strokeWidth='2'
-                            strokeLinecap='round'
-                            strokeLinejoin='round'
-                            aria-hidden='true'
-                        >
-                            <path d='M8 3H5a2 2 0 0 0-2 2v3' />
-                            <path d='M21 8V5a2 2 0 0 0-2-2h-3' />
-                            <path d='M3 16v3a2 2 0 0 0 2 2h3' />
-                            <path d='M16 21h3a2 2 0 0 0 2-2v-3' />
-                        </svg>
-                    )}
-                </button>
-
                 <div className='save-menu-container'>
                     <button
                         id='save-menu-trigger'
@@ -192,6 +146,52 @@ export default function ResultActions({
                         </div>
                     )}
                 </div>
+
+                <button
+                    className='action-button action-button-expand'
+                    onClick={onToggleExpanded}
+                    title={isExpanded ? t.collapseResult : t.expandResult}
+                    aria-label={isExpanded ? t.collapseResult : t.expandResult}
+                    type='button'
+                >
+                    {isExpanded ? (
+                        <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            width='24'
+                            height='24'
+                            viewBox='0 0 24 24'
+                            fill='none'
+                            stroke='currentColor'
+                            strokeWidth='2'
+                            strokeLinecap='round'
+                            strokeLinejoin='round'
+                            aria-hidden='true'
+                        >
+                            <path d='M8 3v3a2 2 0 0 1-2 2H3' />
+                            <path d='M21 8h-3a2 2 0 0 1-2-2V3' />
+                            <path d='M3 16h3a2 2 0 0 1 2 2v3' />
+                            <path d='M16 21v-3a2 2 0 0 1 2-2h3' />
+                        </svg>
+                    ) : (
+                        <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            width='24'
+                            height='24'
+                            viewBox='0 0 24 24'
+                            fill='none'
+                            stroke='currentColor'
+                            strokeWidth='2'
+                            strokeLinecap='round'
+                            strokeLinejoin='round'
+                            aria-hidden='true'
+                        >
+                            <path d='M8 3H5a2 2 0 0 0-2 2v3' />
+                            <path d='M21 8V5a2 2 0 0 0-2-2h-3' />
+                            <path d='M3 16v3a2 2 0 0 0 2 2h3' />
+                            <path d='M16 21h3a2 2 0 0 0 2-2v-3' />
+                        </svg>
+                    )}
+                </button>
             </div>
         </div>
     );
